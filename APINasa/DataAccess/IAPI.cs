0a2b28e0c88cf54e2e04ed7aabeb57706b8e45a0
Daniel Obrero Quero <daniel.obrero@atmira.com>
2022-07-13 12:44:36 +0200

    Instalación de paquete nugget de Json, creación de carpeta data access y creación de clase API y interfaz de esta, con las funciones respectivas para la obtención de los datos de la API de la Nasa.

---
+++ APINasa/DataAccess/IAPI.cs
@@ -0,0 +1,11 @@
+﻿using APINasa.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace APINasa.DataAccess
+{
+    public interface IAPI
+    {
+        Task<List<Meteorito>> Obtenertop3(int dias);
+    }
+}
