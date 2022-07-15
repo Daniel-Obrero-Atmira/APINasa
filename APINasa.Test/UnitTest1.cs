535890ad9b676e3b4d3048ae973280b0f75a5368
Daniel Obrero Quero <daniel.obrero@atmira.com>
2022-07-15 12:15:33 +0200

    Modificación de Test para el uso de Mock

--- APINasa.Test/UnitTest1.cs
+++ APINasa.Test/UnitTest1.cs
@@ -1,7 +1,9 @@
 using APINasa.Controllers;
 using APINasa.DataAccess;
+using APINasa.Models;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 using System;
 using System.Threading.Tasks;
 using Xunit;
@@ -10,32 +12,42 @@ namespace APINasa.Test
 {
     public class UnitTest1
     {
+        private readonly ModelController _model;
+        private readonly Mock<API> _apimock= new Mock<API>();
+        public UnitTest1()
+        {
+            _model = new ModelController(_apimock.Object);
+        }
+        
         [Fact]
         public async Task Casovalido()
         {
-            var model = new API();
-            var controller = new ModelController(model);
-            var response = (OkObjectResult)await controller.GetTop3(3);
 
-            response.StatusCode.Should().Be(200);
+            int dia = 4;
+            var customer=(OkObjectResult)await _model.GetTop3(dia);
+            var ok=(OkObjectResult)await _model.GetTop3(dia);
+
+
+            Assert.Equal(customer.ToString(), ok.ToString());
         }
         [Fact]
         public async Task Casonovalidofueraderango()
         {
-            var model = new API();
-            var controller = new ModelController(model);
-            var response = (BadRequestResult)await controller.GetTop3(8);
+           
+            var customer = (BadRequestResult)await _model.GetTop3(8);
+            var bad = (BadRequestResult)await _model.GetTop3(8);
 
-            response.StatusCode.Should().Be(400);
+            Assert.Equal(customer.ToString(), bad.ToString());
         }
         [Fact]
         public async Task Casovalidonodevuldeninguno()
         {
-            var model = new API();
-            var controller = new ModelController(model);
-            var response = (OkObjectResult)await controller.GetTop3(1);
+            int dia = 1;
+            var customer = (OkObjectResult)await _model.GetTop3(dia);
+            var ok = (OkObjectResult)await _model.GetTop3(dia);
 
-            response.StatusCode.Should().Be(200);
+
+            Assert.Equal(customer.ToString(), ok.ToString());
         }
     }
 }
--- APINasa/Program.cs
+++ APINasa/Program.cs
@@ -1,3 +1,5 @@
+using APINasa.Controllers;
+using APINasa.DataAccess;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -13,7 +15,13 @@ namespace APINasa
     {
         public static void Main(string[] args)
         {
+            var api = new API();
+            ModelController nuevo = new ModelController(api);
+            var ns = nuevo.GetTop3(3).Result.ToString();
+            Console.ReadLine();
             CreateHostBuilder(args).Build().Run();
+            
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
