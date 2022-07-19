fadce38eff8834035bc8029385b2363210ac601e
Daniel Obrero Quero <daniel.obrero@atmira.com>
2022-07-19 12:02:31 +0200

    Modificación de pruebas

--- APINasa.Test/UnitTest1.cs
+++ APINasa.Test/UnitTest1.cs
@@ -5,6 +5,7 @@ using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -23,12 +24,27 @@ namespace APINasa.Test
         public async Task Casovalido()
         {
 
-            int dia = 4;
-            var customer=(OkObjectResult)await _model.GetTop3(dia);
-            var ok=(OkObjectResult)await _model.GetTop3(dia);
+            List<Meteorito> meteoros = new List<Meteorito>();
+            for (int i = 0; i < 3; i++)
+            {
+                Meteorito meteorito = new Meteorito()
+                {
+                    _nombre = "algo",
+                    _diametro = 1.2F,
+                    _fecha="0120329239",
+                    _planeta="Earth",
+                    _velocidad="23423"
+                };
+                meteoros.Add(meteorito);
+            }
+
+            var customer=Task.FromResult(meteoros);
+            var result = _apimock.Setup(x => x.Obtenertop3(3)).Returns(customer);
+
+            var ok = _apimock.Object.Obtenertop3(3);
 
 
-            Assert.Equal(customer.ToString(), ok.ToString());
+            Assert.Equal(result.ToString(), ok.ToString());
         }
         [Fact]
         public async Task Casonovalidofueraderango()
